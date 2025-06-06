4773c44b0234557645e1c0e747f515c62ab72d02
Jomuse05 <johnnymuniz2005@gmail.com>
2025-06-06 19:31:26 +0200

    Solucionat Rotacio De Torre
    Falta Millorar Moviment Flame

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -1,61 +1,62 @@
 extends Node2D
 
-const SPEED := 30.0
-const TILE_SIZE := 8
-const TILES_WIDE := 7
-const TOWER_WIDTH := TILE_SIZE * TILES_WIDE  # 56
+const TOWER_WIDTH := 56  # Ajusta este valor al ancho real de tu torre (en píxeles)
 
+@onready var Player := $"../slime"
+@onready var Torre1 := $Torre1
+@onready var Torre2 := $Torre2
+@onready var Torre3 := $Torre3
+@onready var enemy := $Falme
 
-@onready var Background: TileMapLayer = $Scene/SubViewport/TowerTileMap
-@onready var Torre1: Node2D = $Torre1
-@onready var Torre2: Node2D = $Torre2
-@onready var Torre3: Node2D = $Torre3
-
-var Left: Node2D
-var Mid: Node2D
-var Right: Node2D
+var Left
+var Mid
+var Right
 
 func _ready() -> void:
+	# Posiciona las torres al inicio
 	Left = Torre1
 	Mid = Torre2
 	Right = Torre3
+	
+	Left.position.x = -TOWER_WIDTH
+	Mid.position.x = 0
+	Right.position.x = TOWER_WIDTH
 
-func _physics_process(delta: float) -> void:
-	var direction := -int(Input.get_axis("ui_left", "ui_right"))
-	if direction:
-		for t in [Background]:
-			t.position.x += direction * SPEED * delta
-		check_wrap()
+func _process(delta: float) -> void:
+	check_wrap()
+	check_enemy_wrap()
 
 func check_wrap() -> void:
-	if Background.position.x < TILE_SIZE:
-		Background.position.x += TILE_SIZE
-	elif Background.position.x > TILE_SIZE:
-		Background.position.x -= TILE_SIZE
+	var px = Player.global_position.x
 	
-	"""
-	
-	
-	# Si la torre de la derecha se sale por la izquierda, reubícala al final
-	if Right.position.x < 16+8:
-		print(Right.position.x)
+	# Si el jugador pasa el borde derecho del Mid
+	if px > Mid.position.x + TOWER_WIDTH / 2:
+		# Mover la torre Left a la derecha de Right
 		Left.position.x = Right.position.x + TOWER_WIDTH
-		rotate_parts(true)
-	# Si la torre de la izquierda se sale por la derecha, reubícala al principio
-	elif Left.position.x > 8:
-		print(Left.position.x)
+		_rotate_right()
+	
+	# Si el jugador pasa el borde izquierdo del Mid
+	elif px < Mid.position.x - TOWER_WIDTH / 2:
+		# Mover la torre Right a la izquierda de Left
 		Right.position.x = Left.position.x - TOWER_WIDTH
-		rotate_parts(false)
+		_rotate_left()
 
-func rotate_parts(direction: bool) -> void:
-	if direction:
-		var aux = Left
-		Left = Mid
-		Mid = Right
-		Right = aux
-	else:
-		var aux = Right
-		Right = Mid
-		Mid = Left
-		Left = aux
-"""
+func check_enemy_wrap():
+	# Si el enemigo sale por la derecha, reaparece a la izquierda
+	if enemy.global_position.x > TOWER_WIDTH * 1.5:
+		enemy.global_position.x -= TOWER_WIDTH * 3
+	# Si sale por la izquierda, reaparece a la derecha
+	elif enemy.global_position.x < -TOWER_WIDTH * 1.5:
+		enemy.global_position.x += TOWER_WIDTH * 3
+
+func _rotate_right():
+	var temp = Left
+	Left = Mid
+	Mid = Right
+	Right = temp
+
+func _rotate_left():
+	var temp = Right
+	Right = Mid
+	Mid = Left
+	Left = temp
