2c78b7847c38cf579b4ebf8d1360003506460c83
leah <u1995362@campus.udg.edu>
2025-06-06 13:24:40 +0200

    Slime movement

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -4,7 +4,7 @@ extends Avatar
 @onready var animation_state=  animation_tree.get("parameters/playback")
 @export var slime_scene: PackedScene
 
-const SPEED : float = 64
+const SPEED : float = 30.0
 const JUMP_DISTANCE : float = 3 * 8
 const JUMP_TIME : float = 0.5
 const JUMP_SPEED : float = ( -2 * JUMP_DISTANCE ) / ( JUMP_TIME ) 
@@ -27,80 +27,53 @@ func _ready() -> void:
 	$CoyoteTimer.wait_time = coyote_frames / 60.0
 
 func _physics_process(delta: float) -> void:
-	if is_clone:
-		return
-	
-	var current_direction = int(Input.get_axis("ui_left", "ui_right"))
-	if current_direction: direction = current_direction
-	
-	jump_controller(delta)
-	position_controller(delta)
+	if is_clone: return
+	input_controller(delta)
 	update_animation()
-	
 	move_and_slide()
-	sync_clone(delta)
 
-func _process(delta):
-	if is_clone:
-		return
+func input_controller(delta: float) -> void:
 	
+	movement_controller(delta)
+
 	if Input.is_action_just_pressed("split") and clone == null:
-		split_slime()
+		split_controller(delta)
+	
 
-func update_animation() -> void:
-	if not is_on_floor():
-		if velocity.y > 0:
-			animation_state.travel("Jump")
-			animation_tree.set("parameters/Jump/blend_position", direction)
-			return
-		else:
-			animation_state.travel("Fall")
-			animation_tree.set("parameters/Fall/blend_position", direction)
-			return
-	animation_state.travel("Iddle")
-	animation_tree.set("parameters/Iddle/blend_position", direction)
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	position = spawn_position
-
-func _on_coyote_timer_timeout():
-	coyote = false
-	$CoyoteTimer.stop()
-
-
-func jump_controller(delta: float) -> void:
+func movement_controller(delta: float) -> void:
+	velocity.x = 0
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 		if !jumping and last_floor and !coyote:
 			coyote = true
 			last_floor = false
 			$CoyoteTimer.start()
-	else:
+	elif jumping:
 		jumping = false
 		last_floor = true
 		
+	# Handle movement
+	
+	var current_direction = int(Input.get_axis("ui_left", "ui_right"))
+	if current_direction: 
+		direction = current_direction
+		velocity.x = direction * SPEED
+	
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
 		velocity.y = JUMP_SPEED
 		jumping = true
 
-func position_controller(delta: float) -> void:
-	velocity.x = (spawn_position.x - position.x)
 
-func split_slime():
-	if slime_scene == null:
-		return
-	
+func split_controller(delta: float) -> void:
 	clone = slime_scene.instantiate()
 	clone.position = position + Vector2(16, 0)
 	clone.is_clone = true # 🔁 Indica que és un clon
 	get_parent().add_child(clone)
+	sync_clone(delta)
 
 func sync_clone(delta: float) -> void:
-	if clone == null:
-		return
-	
 	clone.velocity.x = -velocity.x
 	
 	clone.velocity.y += GRAVITY * delta
@@ -124,3 +97,24 @@ func sync_clone(delta: float) -> void:
 	else:
 		clone_anim_state.travel("Iddle")
 		clone_anim_tree.set("parameters/Iddle/blend_position", clone_direction)
+
+
+func update_animation() -> void:
+	if not is_on_floor():
+		if velocity.y > 0:
+			animation_state.travel("Jump")
+			animation_tree.set("parameters/Jump/blend_position", direction)
+			return
+		else:
+			animation_state.travel("Fall")
+			animation_tree.set("parameters/Fall/blend_position", direction)
+			return
+	animation_state.travel("Iddle")
+	animation_tree.set("parameters/Iddle/blend_position", direction)
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	position = spawn_position
+
+func _on_coyote_timer_timeout():
+	coyote = false
+	$CoyoteTimer.stop()
---
+++ scripts/tower.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+const SPEED := 30.0
+const TILE_SIZE := 8
+const TILES_WIDE := 7
+const TOWER_WIDTH := TILE_SIZE * TILES_WIDE  # 56
+
+
+@onready var Background: TileMapLayer = $Scene/SubViewport/TowerTileMap
+@onready var Torre1: Node2D = $Torre1
+@onready var Torre2: Node2D = $Torre2
+@onready var Torre3: Node2D = $Torre3
+
+var Left: Node2D
+var Mid: Node2D
+var Right: Node2D
+
+func _ready() -> void:
+	Left = Torre1
+	Mid = Torre2
+	Right = Torre3
+
+func _physics_process(delta: float) -> void:
+	var direction := -int(Input.get_axis("ui_left", "ui_right"))
+	if direction:
+		for t in [Background]:
+			t.position.x += direction * SPEED * delta
+		check_wrap()
+
+func check_wrap() -> void:
+	if Background.position.x < TILE_SIZE:
+		Background.position.x += TILE_SIZE
+	elif Background.position.x > TILE_SIZE:
+		Background.position.x -= TILE_SIZE
+	
+	"""
+	
+	
+	# Si la torre de la derecha se sale por la izquierda, reubícala al final
+	if Right.position.x < 16+8:
+		print(Right.position.x)
+		Left.position.x = Right.position.x + TOWER_WIDTH
+		rotate_parts(true)
+	# Si la torre de la izquierda se sale por la derecha, reubícala al principio
+	elif Left.position.x > 8:
+		print(Left.position.x)
+		Right.position.x = Left.position.x - TOWER_WIDTH
+		rotate_parts(false)
+
+func rotate_parts(direction: bool) -> void:
+	if direction:
+		var aux = Left
+		Left = Mid
+		Mid = Right
+		Right = aux
+	else:
+		var aux = Right
+		Right = Mid
+		Mid = Left
+		Left = aux
+"""
