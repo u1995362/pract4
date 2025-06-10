9214e30fa5573c72ac8a7f6327b7b75f92d97603
leah <u1995362@campus.udg.edu>
2025-06-10 18:49:29 +0200

    fixed preasure plate

--- Props/Presure plate/presure_plate.gd
+++ Props/Presure plate/presure_plate.gd
@@ -2,13 +2,10 @@ extends StaticBody2D
 
 @onready var anim = $AnimatedSprite2D
 
-func _ready() -> void:
-	anim.play("Normal")
+var pressed: bool = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if(body is Slime): 
+	if !pressed and body is Slime: 
+		pressed = true
 		Global.emit_pressed()
 		anim.play("Pressed")
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	anim.play("Normal")
